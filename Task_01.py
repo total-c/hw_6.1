fb13431318ca778a451a8ab51a0a492434c12e2e
total-c <ku4erov.a.a@gmail.com>
2021-01-13 17:55:56 +0500

    Программа с обращением к функциям

--- Task_01.py
+++ Task_01.py
@@ -50,3 +50,81 @@ def pintToLiter(value):
 def literToPint(value):
     result = value * 1.7597539863927
     return result
+
+# Написать программу, со следующим интерфейсом: пользователю предоставляется
+# на выбор 12 вариантов перевода(описанных в первой задаче).
+# Пользователь вводит цифру от одного до двенадцати.
+# После программа запрашивает ввести численное значение.
+# Затем программа выдает конвертированный результат.
+# Использовать функции из первого задания.
+# Программа должна быть в бесконечном цикле. Код выхода из программы - “0.
+
+i = 1
+while i > 0:
+    n = int(input('Введите выбранную цифру для начала конвертации: '))
+    if n == 1:
+        value = int(input('Введите количество дюймов: '))
+        if __name__ == '__main__':
+            result = inchToSm(value)
+            print(result, 'см')
+    elif n == 2:
+        value = int(input('Введите количество сантиметров: '))
+        if __name__ == '__main__':
+            result = smToInch(value)
+            print(result, 'дюймов')
+    elif n == 3:
+        value = int(input('Введите количество миль: '))
+        if __name__ == '__main__':
+            result = mileToKm(value)
+            print(result, 'км')
+    elif n == 4:
+        value = int(input('Введите количество километров: '))
+        if __name__ == '__main__':
+            result = kmToMile(value)
+            print(result, 'миль')
+    elif n == 5:
+        value = int(input('Введите количество фунтов: '))
+        if __name__ == '__main__':
+            result = poundToKg(value)
+            print(result, 'кг')
+    elif n == 6:
+        value = int(input('Введите количество килограммов: '))
+        if __name__ == '__main__':
+            result = kgToPound(value)
+            print(result, 'фунтов')
+    elif n == 7:
+        value = int(input('Введите количество унций: '))
+        if __name__ == '__main__':
+            result = ounceToGr(value)
+            print(result, 'гр')
+    elif n == 8:
+        value = int(input('Введите количество граммов: '))
+        if __name__ == '__main__':
+            result = grToOunce(value)
+            print(result, 'унций')
+    elif n == 9:
+        value = int(input('Введите количество галлонов: '))
+        if __name__ == '__main__':
+            result = gallonToLiter(value)
+            print(result, 'литров')
+    elif n == 10:
+        value = int(input('Введите количество литров: '))
+        if __name__ == '__main__':
+            result = literToGallon(value)
+            print(result, 'галлонов')
+    elif n == 11:
+        value = int(input('Введите количество пинт: '))
+        if __name__ == '__main__':
+            result = pintToLiter(value)
+            print(result, 'литров')
+    elif n == 12:
+        value = int(input('Введите количество литров: '))
+        if __name__ == '__main__':
+            result = literToPint(value)
+            print(result)
+    elif n > 12:
+            print('Введите число от 1 до 12 или 0, что бы остановить конверер')
+            continue
+    elif n == 0:
+            print('Конвертер остановлен')
+            break
